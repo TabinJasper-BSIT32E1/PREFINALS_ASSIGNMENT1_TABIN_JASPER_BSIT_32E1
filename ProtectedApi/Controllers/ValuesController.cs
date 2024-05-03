2bdb1d6e9c94cc28f9310ca2b40d33ed2adf3a2b
TabinJasper-BSIT32E1 <Tabinjasper@gmail.com>
2024-05-04 00:39:11 +0800

    Added Authorization & Protected Controller

---
+++ ProtectedApi/Controllers/ValuesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProtectedApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize] // Requires authentication for all actions in this controller
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet("userinfo")]
+        public IActionResult GetUserInfo()
+        {
+            // Get user information from claims
+            var username = User.Identity.Name;
+
+            // Return user information
+            return Ok(new { Username = username, Section = "YourSection", Course = "YourCourse" });
+        }
+
+        [HttpGet("funfacts")]
+        public IActionResult GetFunFacts()
+        {
+            // Return fun facts about the API creator
+            string[] funFacts = {
+                "Fun fact 1",
+                "Fun fact 2",
+                "Fun fact 3",
+                "Fun fact 4",
+                "Fun fact 5",
+                "Fun fact 6",
+                "Fun fact 7",
+                "Fun fact 8",
+                "Fun fact 9",
+                "Fun fact 10"
+                // Add more fun facts here
+            };
+
+            return Ok(funFacts);
+        }
+    }
+}
