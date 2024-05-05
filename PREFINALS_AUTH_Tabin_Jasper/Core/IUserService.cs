b7c4145dacdab5ca2efed8b2e933882f612a2124
TabinJasper-BSIT32E1 <Tabinjasper@gmail.com>
2024-05-06 02:35:33 +0800

    Added Models folder and some files JwtSettings & Program

--- PREFINALS_AUTH_Tabin_Jasper/Core/IUserService.cs
+++ PREFINALS_AUTH_Tabin_Jasper/Core/IUserService.cs
@@ -1,12 +1,9 @@
-﻿
-using System.Threading.Tasks;
-
-namespace AuthServer.Core
+﻿namespace AuthServer.Core
 {
     public interface IUserService
     {
         Task<User> GetUserAsync(string username);
-        Task<bool> ValidateCredentialsAsync(string username, string password);
+        Task<bool> CreateUserAsync(User user);
 
     }
 }
